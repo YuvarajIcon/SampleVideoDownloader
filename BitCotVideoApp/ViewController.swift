3c25549450a3bd15ede357a5bcc5cd16f5ce5c21
Yuvaraj Selvam <yuvaraj.s@keka.com>
2023-12-13 19:11:56 +0530

    Initial Commit

---
+++ BitCotVideoApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BitCotVideoApp
+//
+//  Created by Yuvaraj Selvam on 13/12/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
